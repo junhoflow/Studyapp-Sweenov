a7a2131c0b18ba85c9012dd57b505f2af74af04a
Junoflow <tikibird@naver.com>
2021-01-11 04:21:34 +0900

    알람 on/off버튼 및 설정화면 레이아웃

--- app/src/main/java/com/example/sweenov/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/sweenov/ui/notifications/NotificationsFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sweenov.R
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class NotificationsFragment : Fragment() {
 
@@ -27,5 +31,21 @@ class NotificationsFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ToggleButton btn = findViewById(R.id.toggleButton2)
+        btn.setOnClickListener(){
+            if(btn.isChecked){
+                Toast.makeText(activity,"알람 ON", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(activity, "알람 OFF", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }
