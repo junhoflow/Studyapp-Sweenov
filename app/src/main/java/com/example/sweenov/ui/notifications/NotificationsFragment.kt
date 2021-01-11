eacf6583c09ba5fc8f8a71e357359bc410f3d3ab
Junho Kim <tikibird@naver.com>
2021-01-11 18:19:11 +0900

    진동 레이아웃 + 클릭 시 진동

--- app/src/main/java/com/example/sweenov/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/sweenov/ui/notifications/NotificationsFragment.kt
@@ -1,11 +1,16 @@
 package com.example.sweenov.ui.notifications
 
+import android.content.Context.VIBRATOR_SERVICE
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -31,6 +36,7 @@ class NotificationsFragment : Fragment() {
         return root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         toggleButton2.setOnCheckedChangeListener{_, isChecked ->
@@ -40,6 +46,18 @@ class NotificationsFragment : Fragment() {
                 Toast.makeText(activity, "알람 OFF!", Toast.LENGTH_LONG).show()
             }
         }
+
+        vibrate_switch.setOnCheckedChangeListener{ _, isChecked ->
+            val vibrator = context?.getSystemService(VIBRATOR_SERVICE) as Vibrator
+            val vibrationEffect = VibrationEffect.createOneShot(100, 100)
+            if(isChecked){
+                vibrator.vibrate(vibrationEffect)
+            } else{
+                vibrator.cancel();
+            }
+
+        }
+
 
 
 
