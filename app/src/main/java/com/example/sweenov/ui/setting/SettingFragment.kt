270a70ef7a603174a8855a820df454f50c2cfce3
someday55 <kgu010@naver.com>
2021-02-17 15:45:03 +0900

    2021-02-17 디자인 업뎃

--- app/src/main/java/com/example/sweenov/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/sweenov/ui/setting/SettingFragment.kt
@@ -1,20 +1,22 @@
 package com.example.sweenov.ui.setting
 
+import android.content.Context.VIBRATOR_SERVICE
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.sweenov.AlarmSetting
-import com.example.sweenov.BuildConfig
-import com.example.sweenov.R
-import com.example.sweenov.loginWithName
+import com.example.sweenov.*
+import kotlinx.android.synthetic.main.activity_alarmsetting.*
 import kotlinx.android.synthetic.main.fragment_setting.*
 
 
@@ -40,18 +42,22 @@ class SettingFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        isAlarm_switch.setOnCheckedChangeListener{_, isChecked ->
-            val vibrator = context?.getSystemService(VIBRATOR_SERVICE) as Vibrator
-            val vibrationEffect = VibrationEffect.createOneShot(100, 50)
-            if(isChecked){
-                vibrator.vibrate(vibrationEffect)
-                Toast.makeText(activity, "알람 ON!", Toast.LENGTH_LONG).show()
-            }else{
-                vibrator.vibrate(vibrationEffect)
-                Toast.makeText(activity, "알람 OFF!", Toast.LENGTH_LONG).show()
-            }
-        }
+        //isAlarm_switch.setOnCheckedChangeListener{_, isChecked ->
+         //   val vibrator = context?.getSystemService(VIBRATOR_SERVICE) as Vibrator
+         //   val vibrationEffect = VibrationEffect.createOneShot(100, 50)
+         //   if(isChecked){
+         //       vibrator.vibrate(vibrationEffect)
+         //       Toast.makeText(activity, "알람 ON!", Toast.LENGTH_LONG).show()
+         //   }else{
+         //       vibrator.vibrate(vibrationEffect)
+         //       Toast.makeText(activity, "알람 OFF!", Toast.LENGTH_LONG).show()
+         //   }
+        //}
+alarmsetting_btn.setOnClickListener {
 
+    val intent1 = Intent(context, AlarmSetting::class.java)
+    startActivity(intent1)
+}
 
 
         //sharedpreference로 설정값 저장
